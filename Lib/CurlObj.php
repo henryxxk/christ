0b773735f4680da62c2db74ca41292368dd19c2d
henry <henryxxk@gmail.com>
2014-12-07 11:17:24 +0800

    commit

--- Lib/CurlObj.php
+++ Lib/CurlObj.php
@@ -6,22 +6,19 @@
  *  @version v1.0
  */
  
- class CurlObj{
-     protected $ttl = 0;
+class CurlObj
+{  
+    protected $ch = null;
+    
+    protected $ttl = 60;
      
-     protected $host = "";
+    function __construct($url = ''){
+        $this->ch = curl_init();
+    }
      
-     protected $port = "";
-     
-     protected $options = array();
-     
-     protected $ch = null;
-     
-     function __construct(){
-         $this->ch = curl_init();
-     }
-     
-     public function get($url, $params){
-         
-     }
- }
+    public function get($url, $params){
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->ttl);
+        curl_setopt($this->ch, CURLOPT_HEADER, false);
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+    }
+}
